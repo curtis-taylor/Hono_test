d037a7063da7f09acca918f0b11f119373d0cb67
curtis-taylor <curtis.t@gmail.com>
2025-08-04 23:03:10 -0400

    1st hono practice commit

---
+++ hono_proj1/my-app/src/learn.js
@@ -0,0 +1,23 @@
+import { Hono } from "hono";
+import { v4 as uuidv4 } from "uuid";
+
+let videos = [];
+
+const app = new Hono();
+
+app.get('/', (c) => {
+    return c.html('<h1>WELCOME C</h1>');
+});
+
+app.post('/video', async(c) => {
+    const {videoName, channelName, duration} = await c.req.json()
+    const newVideo = {
+        id: uuidv4(),
+        videoName,
+        channelName,
+        duration
+    }
+    videos.push(n)
+});
+
+export default app;
