5218215a071b487c1c8361d0d46f3894f9e2a64e
curtis-taylor <curtis.t@gmail.com>
2025-08-05 23:02:00 -0400

    Hono got post to work and pass using hurl.

--- hono_proj1/my-app/src/learn.js
+++ hono_proj1/my-app/src/learn.js
@@ -1,5 +1,7 @@
 import { Hono } from "hono";
 import { v4 as uuidv4 } from "uuid";
+import { Hono } from 'hono';
+import { stream, streamText, streamSSE } from 'hono/streaming';
 
 let videos = [];
 
@@ -17,7 +19,25 @@ app.post('/video', async(c) => {
         channelName,
         duration
     }
-    videos.push(n)
+    videos.push(newVideo);
+    console.log(videos);
+    return c.json(newVideo);
+});
+
+// READ ALL using stream
+
+app.get('/videos', (c) => {
+    
+    return streamText(c, async(stream) => {
+        for(const video of videos){
+            await stream.writeln(JSON.stringify(video));
+            await stream.sleep(2000);
+        }
+    });
+});
+
+app.get('/video/:id', (c) => {
+
 });
 
 export default app;
